2d024b3b99e8d5ba4487253fba057f39775a612a
Elliott Payne <e.payne000@gmail.com>
2020-10-24 13:11:27 -0500

    addRole function working

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+var promisemysql = require("promise-mysql");
 const cTable = require('console.table');
 
-const connection = mysql.createConnection({
+const connectProp = {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "Tigerseye05!6",
     database: "employees_DB"
-});
+};
+const connection = mysql.createConnection(connectProp);
 
 connection.connect(function(err) {
     if (err) throw err;
@@ -43,7 +45,7 @@ function start() {
                     break;
 
                 case "Add New Role" :
-                    addEmployeeRole();
+                    addRole();
                     break;
                 
                 case "Add New Employee" : 
@@ -55,7 +57,7 @@ function start() {
                     break;
                 
                 case "View all Roles" : 
-                    viewEmployeeRoles();
+                    viewRoles();
                     break;
 
                 case "View all Employees" : 
@@ -63,7 +65,7 @@ function start() {
                     break;
 
                 case "Update Employee Role" : 
-                    changeJob();
+                    updateRole();
                     break;
                 
                 default:
@@ -72,5 +74,104 @@ function start() {
             };
         });
 };
+
+function addDepartment(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'department',
+            message: 'Add Department:'
+        }
+    ]).then(answers => {
+        connection.query(
+            "INSERT INTO department SET ?",
+            {
+                name: answers.department,
+            },
+            function(err){
+                if (err) throw err;
+                console.log('New Department added.')
+                start();
+            }
+        );
+    });
+};
+
+function addRole(){
+    let departmentName = [];
+    promisemysql.createConnection(connectProp)
+    .then((dbconnection)=>{
+        return Promise.all([
+            dbconnection.query("SELECT * FROM department")
+        ]);
+    })
+    .then(([department])=>{
+        for(let i = 0; i<department.length; i++) {
+            departmentName.push(department[i].name);
+        }
+        return Promise.all([department]);
+    })
+    .then(([department])=>{
+        inquirer.prompt([
+            {
+                type:'input',
+                name:'role',
+                message:'Add Role:',
+                validate: function(input){
+                    if(input===""){
+                        console.log("Please enter a Role.");
+                        return false;
+                    }else{
+                        return true;
+                    }
+                }
+            },
+            {
+                type:'input',
+                name:'salary',
+                message:'Add Role Salary:',
+                validate: function(value){
+                    if(isNaN(value)===false){
+                        return true;
+                    }else{
+                        console.log("Please enter a Salary.");
+                        return false;
+                    }
+                }
+            },
+            {
+                type:'list',
+                name:'department',
+                message:'What is the Department for this Role:',
+                choices: departmentName
+            }
+        ])
+        .then(answers=>{
+            let departmentID;
+
+                // Assign a department ID based on the user input choice
+            for (var i = 0; i < department.length; i++) {
+                if (answers.department == department[i].name) {
+                    departmentID = department[i].id;
+                }
+            } 
+                
+            connection.query(
+                "INSERT INTO role SET ?",
+                {
+                title: answers.role,
+                salary: answers.salary,
+                department_id: departmentID
+                },
+                function(err) {
+                if (err) throw err;
+                console.log("Employee Role added successfully");
+                start();
+                }
+            );
+        })
+    })
+    
+}
 
 
